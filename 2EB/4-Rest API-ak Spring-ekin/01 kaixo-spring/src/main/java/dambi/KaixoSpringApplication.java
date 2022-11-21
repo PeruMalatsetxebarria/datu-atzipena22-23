1619d8f0e89a0fed4d52e47c21b0418f5b3ae0ae
PeruMalatsetxebarria <113597099+PeruMalatsetxebarria@users.noreply.github.com>
2022-11-21 12:31:43 +0100

    Add files via upload

---
+++ 2EB/4-Rest API-ak Spring-ekin/01 kaixo-spring/src/main/java/dambi/KaixoSpringApplication.java
@@ -0,0 +1,13 @@
+package dambi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaixoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaixoSpringApplication.class, args);
+	}
+
+}
